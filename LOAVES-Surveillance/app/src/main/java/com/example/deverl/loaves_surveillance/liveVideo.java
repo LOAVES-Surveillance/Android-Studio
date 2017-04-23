d7a7bae5e372cba9f76dadc85e4da46e82604312
Mathias <mathias@Matts-MBP.local>
2017-04-23 00:09:55 -0600

    Live Camera in Web View is working in portrait and Landscape. Additional small adjustments will be needed, but so far, this looks pretty sweet '

--- LOAVES-Surveillance/app/src/main/java/com/example/deverl/loaves_surveillance/liveVideo.java
+++ LOAVES-Surveillance/app/src/main/java/com/example/deverl/loaves_surveillance/liveVideo.java
@@ -1,25 +1,30 @@
 package com.example.deverl.loaves_surveillance;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 
 public class liveVideo extends Fragment {
 
-    public liveVideo(){
-
-    }
+    String IPAddress = "10.68.5.132"; //144.39.222.67
+    String myurl = "http://"+IPAddress+"/html/min.php";
+    private WebView myWebView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        return inflater.inflate(R.layout.fragment_live_video, container, false);
+        View v = inflater.inflate(R.layout.fragment_live_video, container, false);
+        myWebView = (WebView) v.findViewById(R.id.myWebView);
+        myWebView.loadUrl(myurl);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        return v;
     }
-
-
 }
